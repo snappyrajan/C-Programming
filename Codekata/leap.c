42e993c55b8785c90f8f31a45d91c9f5f21aad9a
snappyrajan <38651768+snappyrajan@users.noreply.github.com>
2018-04-24 14:52:56 +0530

    Create leap.c

---
+++ Codekata/leap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    scanf("%d",&year);
+    if(year%4 == 0)
+    {
+        if( year%100 == 0)
+        {
+            if ( year%400 == 0)
+                printf("yes");
+            else
+                printf("No");
+        }
+        else
+            printf("yes");
+    }
+    else
+        printf("No");
+    
+    return 0;
+}
