6b11308044c1a9291398b869cbf5318268242ade
snappyrajan <38651768+snappyrajan@users.noreply.github.com>
2018-04-24 14:02:06 +0530

    Create odd or even.c

---
+++ Codekata/odd or even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=1&&n<=100000)
+    {
+        if(n%2==0)
+            printf("Even number");
+        else
+            printf("Odd number");
+    }
+    else
+        printf("Invalid! the number should be within 1lakh");
+}
