1cbf1df36a79503a3427e4e86960244a64f7cea1
snappyrajan <38651768+snappyrajan@users.noreply.github.com>
2018-04-24 12:15:48 +0530

    Update positive.c

--- Codekata/positive.c
+++ Codekata/positive.c
@@ -4,7 +4,12 @@ void main()
   int n;
   scanf("%d"&n);
   if(n>=1&&n<=100000)
-    printf("POSITIVE");
+    if(n>=1)
+         printf("POSITIVE");
+    elseif(n==0)
+         printf("ZERO");
+    else
+         printf("NEGATIVE");
   else
     printf("INVALID");
  }
